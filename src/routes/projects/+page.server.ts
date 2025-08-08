38b993ad89cd08851a8e9fc5ff1b1c03a497bd1f
R3pic <101osc@naver.com>
2025-08-08 12:42:34 +0900

    feat: 프로젝트 상세 정보 api 연동

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -1,13 +1,19 @@
 import { error } from '@sveltejs/kit';
 import { API_URL } from '$env/static/private';
-import type { Project } from '$lib/types';
+import type { ProjectGetResponse } from '$lib/types';
 import type { PageServerLoad } from './$types';
 
 const IMAGE_URL = new URL('project/files', API_URL).toString();
+
 const PAGE_SIZE = 5;
 
-export const load: PageServerLoad = async ({ url }) => {
-  const allProjects = await loadProjects();
+export const load: PageServerLoad = async ({ url, fetch }) => {
+  const response = await fetch('api/project');
+  const body: ProjectGetResponse = await response.json();
+
+  if (!response.ok) error(response.status, body.message);
+
+  const allProjects = body.works;
   const currentPage = getCurrentPage(url);
 
   const maxPage = Math.ceil(allProjects.length / PAGE_SIZE);
@@ -23,21 +29,6 @@ export const load: PageServerLoad = async ({ url }) => {
     maxPage,
   };
 };
-
-async function loadProjects(): Promise<Project[]> {
-  const apiPath = 'project/works';
-  const fullUrl = new URL(apiPath, API_URL);
-
-  const response = await fetch(fullUrl, {
-    method: 'GET',
-  });
-
-  const body = await response.json();
-
-  if (!response.ok) error(response.status, body.message);
-
-  return body.works;
-}
 
 function getCurrentPage(url: URL): number {
   return Number(url.searchParams.get('page')) || 1;
