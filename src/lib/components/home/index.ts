2df690582197183c7307127ce2ead5179a241211
yehwan <105638607+R3pic@users.noreply.github.com>
2025-09-03 19:24:53 +0900

    Revert "resolves #13 - home 화면 api 연동"

--- src/lib/components/home/index.ts
+++ src/lib/components/home/index.ts
@@ -1,8 +1,1 @@
-export {default as VideoManager} from './VideoManager.svelte';
-export {default as SummaryCard} from './SummaryCard.svelte';
-export {default as ReservationsList} from './ReservationsList.svelte';
-export {default as MeetingRoomList} from './MeetingRoomList.svelte';
-export {default as ProjectList} from './ProjectList.svelte';
-export {default as TodayReservations} from './TodayReservations.svelte';
-export {default as RecentProjects} from './RecentProjects.svelte';
-export {default as PinnedNotices} from './PinnedNotices.svelte';
+// 메인 페이지에서 사용되는 컴포넌트
