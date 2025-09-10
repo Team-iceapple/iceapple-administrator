b1667397698ed1ca1338d6e922a11b5a8d0f578c
R3pic <101osc@naver.com>
2025-09-10 18:47:02 +0900

    fix: project api 경로 처리 수정

--- src/lib/config/api.ts
+++ src/lib/config/api.ts
@@ -1,7 +1,7 @@
 import { API_URL } from '$env/static/private';
 
 export const API_ENDPOINTS = {
-  PROJECT_WORK: `${API_URL}/project/works`,
+  PROJECT_WORK: `${API_URL}/admin/project/works`,
   PROJECT_FILE: `${API_URL}/project/files`,
   PLACE_PLACE: `${API_URL}/admin/place/places`,
   PLACE_RESERVATION: `${API_URL}/admin/place/reservations`,
