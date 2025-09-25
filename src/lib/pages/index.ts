5f21c9a977c394599bacd49c9e3a4a39a04a2355
Boyeon-Shin <anna007096@gmail.com>
2025-09-25 14:44:30 +0900

    feat: 로그인 페이지 구현 (#33)

--- src/lib/pages/index.ts
+++ src/lib/pages/index.ts
@@ -2,3 +2,4 @@
 export { default as HomePage } from './home-page.svelte';
 export { default as PlacePage } from './place-page.svelte';
 export { default as ProjectPage } from './project-page.svelte';
+export { default as LoginPage } from './login-page.svelte';
